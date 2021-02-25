dcb4dc75dd965e5698f54d5358412bd503a90b8c
talhasaglam <talhasaglam153@gmail.com>
2021-02-26 01:51:30 +0300

    no message

---
+++ Assets/Scripts/LessonBasic/IlkScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace LessonBasic
+{
+
+
+    public class IlkScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+
+
+       
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
